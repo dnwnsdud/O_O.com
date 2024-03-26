1c7ae109af12b4d40a72dfba8640a821ae424a67
SeungHwanYa <153148080+SeungHwanYa@users.noreply.github.com>
2024-03-26 16:57:35 +0900

    2024-03-26

--- 20240314/src/component/app.jsx
+++ 20240314/src/component/app.jsx
@@ -1,73 +1,9 @@
 import React from 'react';
 import Downpie from './downpie';
 
-export default ()=>{
-        let data = [
-        {
-            name:"iron",
-            value: 68759 + 54657 + 29669 + 16359,
-            down:[
-                {
-                    name:"i",
-                    value:68759,
-                },
-                {
-                    name:"ii",
-                    value:54657,
-                },
-                {
-                    name:"iii",
-                    value:29669,
-                },
-                {
-                    name:"iv",
-                    value:16359,
-                    down:[
-                        {
-                            name:"top",
-                            value:8359
-                        },
-                        {
-                            name:"bottom",
-                            value:80000
-                        }
-                    ]
-                },
-            ]
-        },
-        {
-            name:"bronze",
-            value: 86052+101638+104781+115637,
-            // down:[
-            //     {
-            //         name:"i",
-            //         value:86052,
-            //     },
-            //     {
-            //         name:"ii",
-            //         value:101638,
-            //     },
-            //     {
-            //         name:"iii",
-            //         value:104781,
-            //     },
-            //     {
-            //         name:"iv",
-            //         value:115637,
-            //     },
-            // ]
-        }
-    ];
+export default () => {
+
     return <div>
-        <Downpie
-            emptyRadius={50}
-            data={data}
-            width={900}
-            height={500}
-            dataKey="value"
-            nameKey="name"
-            downKey="down"
-            step={[50, 50, 20]}
-        />
+
     </div>
 }
