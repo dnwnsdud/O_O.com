e70772bce3ec9b0e02772943a57a7a8128ab93fc
SeungHwanYa <153148080+SeungHwanYa@users.noreply.github.com>
2024-03-27 14:58:25 +0900

    2024-03-27
    회원가입 페이지 버튼 기능 구현

--- 20240314/src/pages/Signup.jsx
+++ 20240314/src/pages/Signup.jsx
@@ -1,4 +1,19 @@
-import { Box, Button, Center, FormControl, FormErrorMessage, FormHelperText, FormLabel, Grid, HStack, Input, InputGroup, InputRightElement, Link, Stack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Grid,
+  HStack,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Link,
+  Stack,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 
 export default () => {
@@ -32,4 +47,5 @@ export default () => {
             <Link width="100%" border="1px solid #0B0B0D" borderRadius="10px" textAlign="center" fontWeight="bold" padding="10px 0 " href='#'>로그인 이동</Link>
         </Stack>
     </Center>
-}
+  );
+};
