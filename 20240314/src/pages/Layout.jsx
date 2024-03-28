a90463c74bc4590b1421061733be7a6438398d67
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-03-28 17:30:21 +0900

    2024-03-28
    임시

--- 20240314/src/pages/Layout.jsx
+++ 20240314/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../component/Footer";
+import Footer from "../component/fa";
 import Header from "../component/Header";
 
 export default () => {
