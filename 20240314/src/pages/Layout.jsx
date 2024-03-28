d68b29db167b83c86345e4e391c7eeab91896358
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-03-28 17:31:00 +0900

    2024-03-28
    수정

--- 20240314/src/pages/Layout.jsx
+++ 20240314/src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../component/fa";
+import Footer from "../component/Footer";
 import Header from "../component/Header";
 
 export default () => {
