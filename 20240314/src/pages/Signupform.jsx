54c7c63cca0906a8b817034a80554f3cc572d77d
dongH5233 <153183825+dongH5233@users.noreply.github.com>
2024-03-29 17:24:25 +0900

    2024-03-29
    회원가입 기능 구현

--- 20240314/src/pages/Signupform.jsx
+++ 20240314/src/pages/Signupform.jsx
@@ -1,37 +1,66 @@
 import { Box, Button, Center, FormControl, FormLabel, Grid, Input, Stack } from "@chakra-ui/react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 
 export default () => {
+    let nav = useNavigate()
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [nickname, setnickname] = useState("");
+    
+
+  const onEmailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+  const onNamedHandler = (e) => {
+    setName(e.target.value);
+  };
+  const onNicknameHandler = (e) => {
+    setnickname(e.target.value);
+  };
+
+  const onSubmitHandler = (e) => {
+    //새로고침 방지
+    e.preventDefault();
+  
+    let body = {
+      email: email,
+      name: name,
+      nickname: nickname,
+    };
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        navigate("/");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+
     return <Center>
         <Stack margin="100px 0" padding="50px 50px 60px" borderRadius="10px" width="500px">
             <Box fontSize='30px' padding="0 30px" textAlign="center" fontWeight='bold' marginBottom="10px">회원가입</Box>
             <FormControl>
-                <FormLabel>아이디</FormLabel>
-                <Input type='text' isRequired />
-            </FormControl>
-            <FormControl>
-                <FormLabel>비밀번호</FormLabel>
-                <Input type='password' isRequired />
-            </FormControl>
-            <FormControl>
-                <FormLabel>비밀번호확인</FormLabel>
-                <Input type='password' isRequired />
-            </FormControl>
-            <FormControl>
-                <FormLabel>닉네임</FormLabel>
-                <Input type='text' isRequired />
+                <FormLabel>이름</FormLabel>
+                <Input type='text' onChange={onNamedHandler}/>
             </FormControl>
             <FormControl>
                 <FormLabel>이메일</FormLabel>
                 <Input type='email' isRequired />
             </FormControl>
+            <FormControl>
+                <FormLabel>닉네임</FormLabel>
+                <Input type='text' onChange={onNicknameHandler} />
+            </FormControl>
+          
             <Grid templateColumns="1fr 1fr" gap="5px" marginTop="20px">
-                <Button border="1px solid #0B0B0D" borderRadius="10px">취소</Button>
-                <Button type="submit" border="1px solid #0B0B0D" borderRadius="10px">회원가입</Button>
+                <Button border="1px solid #0B0B0D" borderRadius="10px" onClick={()=>{
+                    nav('/')
+                }}>취소</Button>
+                <Button type="submit" border="1px solid #0B0B0D" borderRadius="10px" onClikck={onSubmitHandler}>회원가입</Button>
             </Grid>
-            <Button border="1px solid #0B0B0D" borderRadius="10px">로그인</Button>
         </Stack>
     </Center>
 }
