b17a9af9df9566ea13b25540adf55588a81c3862
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-04-17 21:42:21 +0900

    2024-04-17
    모델 수정

--- 20240314/src/models/vote.js
+++ 20240314/src/models/vote.js
@@ -1,65 +1,59 @@
 import mongoose from "mongoose";
-export default mongoose.Schema(
-    {
-        category: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        leftSide: {
-            images: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true,
-            },
-            content: {
-                type: String,
-                required: true,
-            },
-            color:{
-                type: String,
-                required: true,
-            },
-            participants:
-                [
-                    {
-                        type: String
-                    }
-                ]
-        },
-        rightSide: {
-            images: {
-                type: String,
-            },
-            title: {
-                type: String,
-                required: true,
-            },
-            content: {
-                type: String,
-                required: true,
-            },
-            color:{
-                type: String,
-                required: true,
-            },
-            participants:
-                [
-                    {
-                        type: String
-                    }
-                ]
-
-        },
-        date: {
-            type: Date,
-            default: Date.now,
-        }
+export default mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  leftSide: {
+    images: {
+      type: String,
     },
-
-)
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    // color:{
+    //     type: String,
+    //     required: true,
+    // },
+    participants: [
+      {
+        type: String,
+      },
+    ],
+  },
+  rightSide: {
+    images: {
+      type: String,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    // color: {
+    //   type: String,
+    //   required: true,
+    // },
+    participants: [
+      {
+        type: String,
+      },
+    ],
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
