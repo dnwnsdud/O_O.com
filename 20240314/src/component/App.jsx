f3a3ff2a7d976473c55cc92768b8d4b50f1160cf
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-04-25 21:11:10 +0900

    수정중

--- 20240314/src/component/App.jsx
+++ 20240314/src/component/App.jsx
@@ -8,7 +8,6 @@ import Boardmodify from "../pages/Boardmodify";
 import CreateVote from "../pages/CreateVote";
 import Layout from "../pages/Layout";
 import LoL from "../pages/LoL";
-import Loading from "../pages/Loading";
 import Notice from "../pages/Notice";
 import NoticeDetail from "../pages/NoticeDetail";
 import NoticeModify from "../pages/NoticeModify";
@@ -75,72 +74,3 @@ export default () => {
     </UserProvider>
   );
 };
-
-//     let data = [
-//     {
-//         name:"iron",
-//         value: 68759 + 54657 + 29669 + 16359,
-//         down:[
-//             {
-//                 name:"i",
-//                 value:68759,
-//             },
-//             {
-//                 name:"ii",
-//                 value:54657,
-//             },
-//             {
-//                 name:"iii",
-//                 value:29669,
-//             },
-//             {
-//                 name:"iv",
-//                 value:16359,
-//                 down:[
-//                     {
-//                         name:"top",
-//                         value:8359
-//                     },
-//                     {
-//                         name:"bottom",
-//                         value:80000
-//                     }
-//                 ]
-//             },
-//         ]
-//     },
-//     {
-//         name:"bronze",
-//         value: 86052+101638+104781+115637,
-//         // down:[
-//         //     {
-//         //         name:"i",
-//         //         value:86052,
-//         //     },
-//         //     {
-//         //         name:"ii",
-//         //         value:101638,
-//         //     },
-//         //     {
-//         //         name:"iii",
-//         //         value:104781,
-//         //     },
-//         //     {
-//         //         name:"iv",
-//         //         value:115637,
-//         //     },
-//         // ]
-//     }
-// ];
-// return <div>
-//     <Downpie
-//         emptyRadius={50}
-//         data={data}
-//         width={900}
-//         height={500}
-//         dataKey="value"
-//         nameKey="name"
-//         downKey="down"
-//         step={[50, 50, 20]}
-//     />
-// </div>
